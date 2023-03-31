c9cf5a8a855afd1922c6a7236173198c2f77a530
naveenmr2001 <naveen.mr@m2pfintech.com>
2023-03-31 12:33:38 +0530

    Initial Commit project

---
+++ src/test/java/com/test/probabilityTest.java
@@ -0,0 +1,19 @@
+package com.test;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class probabilityTest {
+
+    @Nested
+    public class Coin{
+        @Test
+        void toCheckIfProbabilityOfHeadIsEqualsProbabilityOfTails(){
+
+            Probability probability = new Probability();
+
+        }
+
+    }
+
+}
