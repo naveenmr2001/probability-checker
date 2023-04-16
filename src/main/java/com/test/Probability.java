0c966669d946d96591b17fff08fa2642550c5b51
naveenmr2001 <naveen.mr@m2pfintech.com>
2023-04-16 22:13:31 +0530

    Requirement1 Add. Added the findtogetheroccuring logic in the code

--- src/main/java/com/test/Probability.java
+++ src/main/java/com/test/Probability.java
@@ -2,6 +2,28 @@ package com.test;
 
 public class Probability {
 
+    private final double probabilityOfObject;
+    public Probability(double probabilityOfObject){
+        this.probabilityOfObject = probabilityOfObject;
+    }
 
+    public boolean equals(Object object)
+    {
+        if(this == object){
+            return true;
+        }
+        if(this.getClass() != object.getClass()){
+            return false;
+        }
+        Probability probabilityObj = (Probability) object;
+        return probabilityObj.probabilityOfObject == this.probabilityOfObject;
+    }
 
+    public Probability findTogetherOccurring(Probability probabilityOfHeadToss) {
+        return new Probability(this.probabilityOfObject*probabilityOfHeadToss.probabilityOfObject);
+    }
+
+    public Probability complement() {
+        return new Probability(1 - this.probabilityOfObject);
+    }
 }
--- src/test/java/com/test/probabilityTest.java
+++ src/test/java/com/test/probabilityTest.java
@@ -3,17 +3,85 @@ package com.test;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.not;
+
 public class probabilityTest {
 
     @Nested
     public class Coin{
         @Test
-        void toCheckIfProbabilityOfHeadIsEqualsProbabilityOfTails(){
+        void toCheckIfProbabilityOfHeadIsEqualsProbabilityOfTails() {
 
-            Probability probability = new Probability();
+            Probability probabilityOfHeadToss = new Probability(0.5);
 
+            Probability probabilityOfTailToss = new Probability(0.5);
+
+            assertThat(probabilityOfHeadToss, is(equalTo(probabilityOfTailToss)));
+        }
+    }
+    @Nested
+    public class Requirement2{
+
+        @Test
+        void toCheckIfAEventAreOccurringTogetherIfTwoEventOccuringConform(){
+
+            Probability probabilityOfSureOccurringEvent1 = new Probability(1);
+
+            Probability probabilityOfSureOccurringEvent2 = new Probability(1);
+
+            Probability expectedValue = new Probability(1);
+
+            assertThat(probabilityOfSureOccurringEvent1.findTogetherOccurring(probabilityOfSureOccurringEvent2),is(equalTo(expectedValue)));
+
+        }
+        @Test
+        void toCheckIfTwoEventAreOccurringTogether(){
+
+            Probability probabilityOfEvent1 = new Probability(0.5);
+
+            Probability probabilityOfEvent2 = new Probability(0.16);
+
+            Probability exceptedResult = new Probability(0.08);
+
+            assertThat(probabilityOfEvent1.findTogetherOccurring(probabilityOfEvent2),is(equalTo(exceptedResult)));
+        }
+
+        @Test
+        void toCheckIfTwoEventAreOccurringTogetherFalse(){
+
+            Probability probabilityOfEvent1 = new Probability(0.5);
+
+            Probability probabilityOfEvent2 = new Probability(0.16);
+
+            Probability exceptedResult = new Probability(0.1);
+
+            assertThat(probabilityOfEvent1.findTogetherOccurring(probabilityOfEvent2),is(not(equalTo(exceptedResult))));
+        }
+    }
+
+    @Nested
+    public class Requirement3{
+
+        @Test
+        void toReturnProbabilityOfEventNotOccurringCertain(){
+            Probability expectedValue = new Probability(0.5);
+
+            Probability actualValue = new Probability(0.5);
+
+            assertThat(expectedValue.complement(), is(equalTo(actualValue)));
+        }
+
+        @Test
+        void toReturnProbabilityOfEventNotOccurringUnCertain(){
+            Probability expectedValue = new Probability(1);
+
+            Probability actualValue = new Probability(0);
+
+            assertThat(expectedValue.complement(), is(equalTo(actualValue)));
         }
 
     }
-
 }
